b7d9ed7276af3bf501906988c66ec6d27dcee1a6
Óscar Gómez <oscar.gomez.borz@gmail.com>
2016-09-23 18:14:39 +0200

    Initial commit

---
+++ practica1/ordenacion.c
@@ -0,0 +1,32 @@
+/**
+ *
+ * Descripcion: Implementacion de funciones de ordenacion 
+ *
+ * Fichero: ordenacion.c
+ * Autor: Carlos Aguirre
+ * Version: 1.0
+ * Fecha: 16-09-2016
+ *
+ */
+
+
+#include "ordenacion.h"
+
+/***************************************************/
+/* Funcion: InsertSort    Fecha:                   */
+/* Vuestro comentario                              */
+/***************************************************/
+int InsertSort(int* tabla, int ip, int iu)
+{
+  /* vuestro codigo */
+}
+
+/***************************************************/
+/* Funcion: InsertSortInv    Fecha:                   */
+/* Vuestro comentario                              */
+/***************************************************/
+int InsertSortInv(int* tabla, int ip, int iu)
+{
+  /* vuestro codigo */
+}
+
