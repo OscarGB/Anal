edbd31a60837c60144bc3fdd6200eaa49a2296b3
Óscar Gómez <oscar.gomez.borz@gmail.com>
2016-12-15 15:30:54 +0100

    el arreglo de verdad

--- practica3/busqueda.c
+++ practica3/busqueda.c
@@ -266,7 +266,12 @@ int blin_auto(int *tabla,int P,int U,int clave,int *ppos)
     for(i = P; i <= U; i++){
         obs++;
         if(clave == tabla[i]) {
-            *ppos = i-1;
+        	if(i != 0){
+            	*ppos = i-1;
+        	}
+        	else{
+        		*ppos = i;
+        	}
             flag = 1;
             if(i > 0){
             	swap(&tabla[i], &tabla[i-1]);
